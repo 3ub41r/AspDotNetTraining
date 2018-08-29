fb28f96036d3f858d2e83fa4755ce5a1167d474f
zubair <zubairabuhasan@gmail.com>
2018-08-29 16:50:21 +0800

    Add areas

---
+++ AspDotNetTraining/Areas/Student/Controllers/SubjectsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspDotNetTraining.Areas.Student.Controllers
+{
+    [RouteArea("Student")]
+    [RoutePrefix("Subjects")]
+    [Route("{action}")]
+    public class SubjectsController : Controller
+    {
+        // GET: Student/Subjects
+        public ActionResult Index()
+        {
+            return Content("Welcome to /student/subjects");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
